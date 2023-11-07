fad1a0876ded1db146170a0cee99f55bc2ddd7df
Stephan Weißbach <s.weissbach@uni-mainz.de>
2023-11-07 13:56:02 +0100

    Program was crashing when tau was not computable. Now, np.nan

--- synapse_selector/utils/decay_compute.py
+++ synapse_selector/utils/decay_compute.py
@@ -3,17 +3,22 @@ import numpy as np
 
 
 def compute_tau(voltage: np.ndarray) -> tuple[float, float]:
-    '''
+    """
     Fit an exponential curve to calculate the decay constant tau.
-    '''
+    """
     """def exp_decay(t, V0, tau):
         return V0 * np.exp(-t / tau)"""
+
     def exp_decay(t, y0, A, invTau):
         return y0 + A * np.exp(-t * invTau)
+
     if len(voltage) <= 1:
-        return np.nan
+        return (np.nan, np.nan)
     frames = [i for i in range(len(voltage))]
-    params, _ = curve_fit(exp_decay, frames, voltage, maxfev=5000)
+    try:
+        params, _ = curve_fit(exp_decay, frames, voltage, maxfev=5000)
+    except:
+        return (np.nan, np.nan)
     inv_tau = params[2]
-    tau = 1/inv_tau
+    tau = 1 / inv_tau
     return (inv_tau, tau)
