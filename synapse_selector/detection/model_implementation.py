f9327841bc5df43d9e64bd6754401f9a5646d7b1
j-milkovits <j.milkovits.t@posteo.net>
2024-01-04 16:18:45 +0000

    fix model formatting

--- synapse_selector/detection/model_implementation.py
+++ synapse_selector/detection/model_implementation.py
@@ -27,11 +27,12 @@ class PeakDetectionModel(nn.Module):
 
         # Convolutional layers with batch normalization and relu activation
         x1 = F.relu(self.batch_norm1(self.conv1(x)))
-        x2 = F.relu(self.batch_norm2(self.conv2(torch.cat([x,x1], dim=1))))
-        x3 = F.relu(self.batch_norm3(self.conv3(torch.cat([x,x2], dim=1))))
-        x4 = F.relu(self.batch_norm4(self.conv4(torch.cat([x,x3], dim=1))))
-        
+        x2 = F.relu(self.batch_norm2(self.conv2(torch.cat([x, x1], dim=1))))
+        x3 = F.relu(self.batch_norm3(self.conv3(torch.cat([x, x2], dim=1))))
+        x4 = F.relu(self.batch_norm4(self.conv4(torch.cat([x, x3], dim=1))))
+
         # Apply output convolutional layer with 1 channel
-        out = torch.sigmoid(self.output_conv(torch.cat([x,x1,x2,x3,x4], dim=1)))
+        out = torch.sigmoid(self.output_conv(
+            torch.cat([x, x1, x2, x3, x4], dim=1)))
 
         return out[:, -1, :]  # Squeeze the channel dimension for a 1D output
