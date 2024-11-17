7365b4def37e584ae325534b9f64c0d3e39ce81b
Andreas Brilka <174248270+andreasmz@users.noreply.github.com>
2024-11-18 00:27:14 +0100

    add new init file and standalone caller

---
+++ trace_selector/__init__.py
@@ -0,0 +1,29 @@
+from .gui.gui import MainWindow
+from .utils.configuration import gui_settings
+from .detection.model_zoo import ModelZoo
+
+from PyQt6.QtWidgets import QApplication
+import sys
+import os
+from platformdirs import user_data_dir
+import pathlib
+
+
+def main():
+    # get modelzoo directory
+    modelzoo_folder = user_data_dir("trace_selector")
+    # check for available ML models
+    modelzoo = ModelZoo(modelzoo_folder)
+    modelzoo.check_for_updates()
+    # initalize settings
+    settings = gui_settings(modelzoo)
+    # initalize GUI
+    app = QApplication(sys.argv)
+    main = MainWindow(settings)
+    # main = UiWindow(settings)
+    main.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
---
+++ trace_selector_main.py
@@ -0,0 +1,1 @@
+import trace_selector_dev
