e55be94383a637c230b57b7eb1e33fc559f9f4ab
andreasmz <174248270+andreasmz@users.noreply.github.com>
2024-11-21 16:50:42 +0100

    init and main refractoring

--- trace_selector_dev/__init__.py
+++ trace_selector_dev/__init__.py
@@ -1,30 +1,35 @@
+import time
+
+starttime = time.time()
 from .gui.gui import MainWindow
+print("MainWindow", round(time.time()-starttime,3), "s")
 from .utils.configuration import gui_settings
+print("Gui Settings", round(time.time()-starttime,3), "s")
 from .detection.model_zoo import ModelZoo
+print("ModelZoo", round(time.time()-starttime,3), "s")
 
 from PyQt6.QtWidgets import QApplication
 import sys
 from platformdirs import user_data_dir
 
+print("Rest", round(time.time()-starttime,3), "s")
+
 class App:
     main: MainWindow = None
-    def ini(): 
+    def start(): 
         # get modelzoo directory
         modelzoo_folder = user_data_dir("trace_selector")
+
         # check for available ML models
         modelzoo = ModelZoo(modelzoo_folder)
         modelzoo.check_for_updates()
+
         # initalize settings
         settings = gui_settings(modelzoo)
+
         # initalize GUI
         app = QApplication(sys.argv)
         App.main = MainWindow(settings)
         App.main.show()
+        print("Showing", round(time.time()-starttime,3), "s")
         sys.exit(app.exec())
-
-    def start():
-        App.ini()
- 
-
-if __name__ == "__main__":
-    App.start()
